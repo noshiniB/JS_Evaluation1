6ec420b6debf0bc57eca59338ade1bffd3075382
Noshini Bheeman <114381551+noshiniB@users.noreply.github.com>
2022-11-11 16:01:13 +0530

    Update word_reverse.js

--- word_reverse.js
+++ word_reverse.js
@@ -1,9 +1,9 @@
 //Reverse a word in sentence
 
-var s = ["i", "like", "this",
+let s = ["i", "like", "this",
     "program", "very", "much"];
-var ans = "";
-for (var i = 5; i >= 0; i--) {
+let ans = "";
+for (let i = 5; i >= 0; i--) {
     ans += s[i] + " ";
 }
 console.log(ans);
